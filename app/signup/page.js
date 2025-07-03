3261e0873a521c730821220b05c998ca7ccef060
Samarth Mankar <iamsamarth03@gmail.com>
2025-07-03 20:54:27 +0530

    wrapped in suspense

--- app/signup/page.js
+++ app/signup/page.js
@@ -1,5 +1,10 @@
+import { Suspense } from "react";
 import SignupClient from "./SignupClient";
 
 export default function SignupPage() {
-  return <SignupClient />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <SignupClient />
+    </Suspense>
+  );
 }
